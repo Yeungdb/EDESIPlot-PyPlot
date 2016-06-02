7f9683232fc99809f82fa947ee8499f14986eeaf
Darien <darien.yeung@gmail.com>
2016-06-02 16:14:23 -0700

    Modified to use PyPlot instead of Plotly for EDESI
       - Converted to PyPlot for constructing EDESI plots as it support full
         plot configuration for axes and correct spacing.
       - Can innately render png for convenience of publication figures

--- EDESIPlot.py
+++ EDESIPlot.py
@@ -5,13 +5,19 @@ import collections
 import plotly
 import plotly.graph_objs as go
 import plotly.tools as tls
+import plotly.plotly as py
 
 parser = OptionParser()
 parser.add_option('-f', "--filename", help="Filename of file for processing", action="store")
 parser.add_option('-w', "--width", help="Width of line", action="store")
 parser.add_option('-m', "--minFilter", help="Minimum Value for Contour Plot Filter", action="store")
+parser.add_option('-s', "--fontSize", help="Font Size for Plot", action="store")
 parser.add_option('-t', "--threshold", help="Threshold Intensity for Picking MZ in Breakdown Graph", action="store")
 parser.add_option('-o', "--outputFile", help="Name of Output File (enter without .html)", action="store")
+parser.add_option('-k', '--tickDist', help="Distance between each tick", action="store")
+parser.add_option('-r', '--maxRange', help="Max value in the X Axis", action="store")
+parser.add_option('-b', "--breakDown", help="Enable/Disable Breakdown Plots", action="store_true", default=False)
+parser.add_option('-p', "--publication", help="Styles the plot to publication standards", action="store_true", default=False)
 options, args = parser.parse_args()
 
 filename=options.filename
@@ -103,11 +109,11 @@ for i in summed:
             localMaxima = summed[i]
         else:
             #Remove local maximas that are located on the side of large local maximas ie 123.3333: 12, 123.4545: 13, 12,5444: 12, 12.6333: 126, 12.7333: 12
-            print str(i) + " " + str(summed[i]) 
+            #print str(i) + " " + str(summed[i]) 
             breakDownMZ.append(int(float(i)))
     else:
         localMaxima = 0
-print breakDownMZ
+#print breakDownMZ
 
 listofAllTrace = {}
 for mz in breakDownMZ:
@@ -123,7 +129,7 @@ binArr = {}
 for i in range(minVal, maxVal+1):
    binArr[i] = 0
 
-
+TraceMaxInt = 0
 ZMatrix = []
 contourz = contourPlotArr.values()
 for i in contourz:
@@ -132,6 +138,9 @@ for i in contourz:
    ZMatrix.append(binArr.values())
    for mz in breakDownMZ:
        listofAllTrace[mz].append(binArr[mz])
+       print str(max(binArr)) + " " + str(TraceMaxInt)
+       if(max(binArr) > TraceMaxInt):
+           TraceMaxInt = max(binArr) 
    binArr = dict.fromkeys(binArr, 0)
 
 #################################
@@ -140,7 +149,7 @@ for i in contourz:
 newArr = collections.OrderedDict(sorted(tArr.items()))
 
 
-
+"""
 ##################################
 #PLOTLY PLOTS
 
@@ -178,52 +187,179 @@ trace1 = go.Contour(
 )
 
 data = [trace0, trace1]
-for mz in breakDownMZ:
-    trace = go.Scatter(
-       x = listofAllTrace[mz],
-       name = "" + str(mz),
-       xaxis='x2',
-       text="" + str(mz) + "m/z",
-       textposition = 'top',
-       line = dict(
-          width = int(options.width)
-       )
-    )
-    data.append(trace)
 
-layout = go.Layout(
-        showlegend=False,
-        xaxis=dict(
-            title='m/z',
-            domain=[0, 0.78],
-            showgrid=False,
-            zeroline=False
-            ),
-        yaxis=dict(
-            title='Collision Energy(V)',
-            domain=[0, 0.78],
-            showgrid=False,
-            zeroline=False
-            ),
-        margin=dict(
-            t=50,
-            r=100
-            ),
-        xaxis2=dict(
-            title='Intensity',
-            domain=[0.78,1],
-            showgrid=False,
-            zeroline=False
-        ),
-        yaxis2=dict(
-            title='Intensity',
-            domain=[0.78,1],
-            showgrid=False,
-            zeroline=False
+showBreakdown = options.breakDown
+if(showBreakdown == True):
+    for mz in breakDownMZ:
+        trace = go.Scatter(
+           x = listofAllTrace[mz],
+           name = "" + str(mz),
+           xaxis='x2',
+           text="" + str(mz) + "m/z",
+           textposition = 'top',
+           line = dict(
+              width = int(options.width)
+           )
         )
-    )
+        data.append(trace)
 
+
+#tickDist = int(options.tickDist)
+#maxRange = int(options.maxRange)
+#print maxIntensity
+#print tickDist
+marginStyle=go.Margin(r=50, t=50, pad=4)
+marginStyleBreakdown=go.Margin(l=50, r=50, t=50, pad=4)
+#xAxisStyleMZ=dict(title='m/z',showgrid=False,ticks='outside',zeroline=True, range=[0, maxRange])
+#yAxisStyleCOLENG=dict(title='Collision Energy(V)',showgrid=False,ticks='outside', showline=True, zeroline=False)
+#xAxisStyleINT=dict(title='Intensity',domain=[0.78,1],ticks='outside',showgrid=False,zeroline=True, dtick=tickDist)
+#yAxisStyleINT=dict(title='Intensity',domain=[0.78,1],ticks='outside',showgrid=False,showline=True, zeroline=False)
+
+
+
+yAxisStyleCOLENG=dict(title='Collision Energy(V)',showgrid=False,ticks='outside',zeroline=False)
+yAxisStyleINT=dict(title='Intensity',domain=[0.78,1],ticks='outside',showgrid=False, zeroline=False)
+xAxisStyleMZ=dict(title='m/z',showgrid=False,ticks='outside',zeroline=True)
+xAxisStyleINT=dict(title='Intensity',domain=[0.78,1],ticks='outside',showgrid=False,zeroline=True)
+
+#fontSize = int(options.fontSize)
+#fontStyle=dict(family='Arial',size=fontSize,color='#000')
+widthStyle=400
+widthToheightRatio=float(1280/1024) #height/width
+pubStyle = bool(options.publication)
+if(pubStyle):
+    if(showBreakdown):
+        xAxisStyleMZ['domain'] = (0, 0.78)
+        yAxisStyleCOLENG['domain'] = (0, 0.78)
+        layout = go.Layout(
+                #font=fontStyle,
+                width=widthStyle,
+                height=widthStyle*widthToheightRatio,
+                showlegend=False,
+                xaxis=xAxisStyleMZ,
+                yaxis=yAxisStyleCOLENG,
+                margin=marginStyle,
+                xaxis2=xAxisStyleINT,
+                yaxis2=yAxisStyleINT
+            )
+    else:
+        layout = go.Layout(
+                #font=fontStyle,
+                width=widthStyle,
+                height=widthStyle*widthToheightRatio,
+                showlegend=False,
+                xaxis=xAxisStyleMZ,
+                yaxis=yAxisStyleCOLENG,
+                margin=marginStyle,
+                yaxis2=yAxisStyleINT
+            )
+else:
+    if(showBreakdown):
+        xAxisStyleMZ['domain'] = (0, 0.78)
+        yAxisStyleCOLENG['domain'] = (0, 0.78)
+        layout = go.Layout(
+                #font=fontStyle,
+                showlegend=False,
+                xaxis=xAxisStyleMZ,
+                yaxis=yAxisStyleCOLENG,
+                margin=marginStyle,
+                xaxis2=xAxisStyleINT,
+                yaxis2=yAxisStyleINT
+            )
+    else:
+        layout = go.Layout(
+                #font=fontStyle,
+                showlegend=False,
+                xaxis=xAxisStyleMZ,
+                yaxis=yAxisStyleCOLENG,
+                margin=marginStyle,
+                yaxis2=yAxisStyleINT
+            )
 
 fig = go.Figure(data=data, layout=layout)
 
-plotly.offline.plot(fig, validate=False, filename='{OUTFILE}.html'.format(OUTFILE=options.outputFile))
+#plotly.offline.plot(fig, validate=False, filename='{OUTFILE}.html'.format(OUTFILE=options.outputFile))
+outFileDir = "/".join(options.outputFile.split('/')[:-1])
+plotly.offline.plot(fig, validate=False, filename='{OUTDIR}/temp.html'.format(OUTDIR=outFileDir))
+#py.image.save_as(fig, filename='EDESI.png')
+#py.image.ishow(fig, 'png', scale=1)
+Readfile = open('{OUTDIR}/temp.html'.format(OUTDIR=outFileDir), 'r')
+Writefile = open('{OUTFILE}.html'.format(OUTFILE=options.outputFile), 'w+')
+lines = Readfile.readlines()
+for i in lines:
+   if('Export to plot.ly' in i):
+      i = i.replace("Export to plot.ly", "")
+      Writefile.write(i)
+      continue
+   Writefile.write(i)
+Readfile.close()
+Writefile.close()
+
+"""
+
+import matplotlib
+import matplotlib.gridspec as gridspec
+import matplotlib.pyplot as plt
+from scipy import *
+from math import log10, floor
+
+def round1sig(yTick):
+    for i in range(0, len(yTick)):
+        #stackoverflow: 3410976
+        if (yTick[i] == 0):
+            continue
+        yTick[i] = round(yTick[i], -int(floor(log10(abs(yTick[i])))))
+    return yTick
+
+def Valueround1sig(yTick):
+    return round(yTick, -int(floor(log10(abs(yTick)))))
+
+showBreakdown = options.breakDown
+line_colours = ('BlueViolet', 'Crimson', 'ForestGreen', 'Indigo', 'Tomato', 'Maroon')
+
+minFilter = int(options.minFilter)
+print minFilter
+levels = arange(minFilter, max(array(ZMatrix).max(axis=1)), 6)
+#line_widths = (1, 1.5, 2, 2.5, 3, 3.5)
+
+subplotVal = 1
+if(showBreakdown == True):
+    subplotVal = 2
+
+plt.figure()
+gs1 = gridspec.GridSpec(subplotVal,2,width_ratios=[3,1], height_ratios=[1,3])
+gs1.update(wspace=0.025, hspace=0.025)
+
+
+ax2 = plt.subplot(gs1[subplotVal])
+ContourPlot = ax2.contour(binArr.keys(), contourPlotArr.keys(), ZMatrix, levels, colors = 'k')
+ContouryTick = arange(0, Valueround1sig(max(contourPlotArr.keys()))+10, 10)
+ax2.set_yticks(ContouryTick[:-1])
+ContourxTick = arange(0, Valueround1sig(max(binArr.keys()))+250, 250)
+print ContourxTick
+ax2.set_xticks(ContourxTick[:-1])
+ax2.set_ylabel('Collision Energy (V)')
+ax2.set_xlabel('m/z')
+
+
+ax1 = plt.subplot(gs1[0], sharex=ax2)
+SummedFullScan = ax1.plot(summed.keys(), summed.values())
+plt.title('Summed Full Scan')
+plt.setp(ax1.get_xticklabels(), visible=False)
+ax1.set_ylabel('Intensity')
+#stackoverflow: 11244514
+ax1yTick = arange(0, Valueround1sig(max(summed.values()))+100000, 100000) 
+ax1.set_yticks(ax1yTick)
+
+
+showBreakdown = options.breakDown
+if(showBreakdown == True):
+    ax3 = plt.subplot(gs1[3], sharey=ax2)
+    for mz in breakDownMZ:
+        ax3.plot(listofAllTrace[mz], contourPlotArr.keys())
+    plt.setp(ax3.get_yticklabels(), visible=False)
+    BreakDownInt = arange(0, TraceMaxInt, 1000)
+    print TraceMaxInt
+    ax3.set_xticks(BreakDownInt)
+
+plt.savefig('Contour.png')
