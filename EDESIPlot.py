639c7377c0ff7eaa7b355bfd1b4a93c942681aed
Darien <darien.yeung@gmail.com>
2016-07-06 16:23:33 -0700

    Updated EDESI Plot by removing unused switches

--- EDESIPlot.py
+++ EDESIPlot.py
@@ -1,30 +1,28 @@
-#!/usr/bin/python2.7
+#!/USR/bin/python2.7
 
 from optparse import OptionParser
 import collections
-import plotly
-import plotly.graph_objs as go
-import plotly.tools as tls
-import plotly.plotly as py
+import matplotlib
+import matplotlib.gridspec as gridspec
+import matplotlib.pyplot as plt
+from scipy import *
+from math import *
 
 parser = OptionParser()
 parser.add_option('-f', "--filename", help="Filename of file for processing", action="store")
-parser.add_option('-w', "--width", help="Width of line", action="store")
 parser.add_option('-m', "--minFilter", help="Minimum Value for Contour Plot Filter", action="store")
-parser.add_option('-s', "--fontSize", help="Font Size for Plot", action="store") 
 parser.add_option('-t', "--threshold", help="Threshold Intensity for Picking MZ in Breakdown Graph", action="store")
-parser.add_option('-o', "--outputFile", help="Name of Output File (enter without .html)", action="store")
-parser.add_option('-k', '--tickDist', help="Distance between each tick", action="store")
-parser.add_option('-r', '--maxRange', help="Max value in the X Axis", action="store")
+parser.add_option('-o', "--outputFile", help="Name of Output File without extension", action="store")
 parser.add_option('-b', "--breakDown", help="Enable/Disable Breakdown Plots", action="store_true", default=False)
-parser.add_option('-p', "--publication", help="Styles the plot to publication standards", action="store_true", default=False)
+parser.add_option('-z', "--zoom", help="Enable/Disable Zoom", action="store_true", default=False)
+
 options, args = parser.parse_args()
 
 filename=options.filename
 
 def anon(x):
+   #Used for testing lambda functions
    print x
-
 
 def GraphCleanUp(ax):
     ax.spines['right'].set_visible(False)
@@ -32,8 +30,34 @@ def GraphCleanUp(ax):
     ax.get_xaxis().tick_bottom()
     ax.get_yaxis().tick_left()
 
+def RoundToTen(val):
+    return int(ceil(val/10.0))*10
+
+def RoundToTenFloor(val):
+    return int(floor(val/10.0))*10
+
+def RoundToHundred(val):
+    return int(ceil(val/100.0))*100
+
+def round1sig(yTick):
+    for i in range(0, len(yTick)):
+        #stackoverflow: 3410976
+        if (yTick[i] == 0):
+            continue
+        yTick[i] = round(yTick[i], -int(floor(log10(abs(yTick[i])))))
+    return yTick
+
+def Valueround1sig(yTick):
+    return round(yTick, -int(floor(log10(abs(yTick)))))
 file = open(filename, 'r')
 lines = file.readlines()
+
+def normalizeMSSpectrum(mzList, maxValue):
+    relInt = []
+    for i in mzList:
+       relInt.append((float(i)/float(maxValue))*100)
+       #print ((float(i)/float(maxValue))*100)
+    return relInt
 
 #Loop Var initialization
 arr = {} #Time to ColToSpec
@@ -50,6 +74,7 @@ minVal = 1000
 maxVal = 0
 maxIntensity = 0
 
+#Parse through text file to find all the relevant parameters for the experiment
 for i in range(67, len(lines)):
     SpecDict = {}
     if "scan start time" in lines[i]:
@@ -100,11 +125,9 @@ summed = collections.OrderedDict(sorted(c.items(), key = lambda x: float(x[0])))
 #Constructing Z axis matrix for Contour Plot
 contourPlotArr = collections.OrderedDict(sorted(contourArr.items()))
 
-
-
 #################################3
 #MAKE Breakdown Graphs
-
+#ie. Reconstructed SIM traces of the Reactants, Intermediates and Products
 
 threshold = options.threshold
 breakDownMZ = []
@@ -115,18 +138,14 @@ for i in summed:
             localMaxima = summed[i]
         else:
             #Remove local maximas that are located on the side of large local maximas ie 123.3333: 12, 123.4545: 13, 12,5444: 12, 12.6333: 126, 12.7333: 12
-            #print str(i) + " " + str(summed[i]) 
+            #Essentially, try to remove peaks in peaks
             breakDownMZ.append(int(float(i)))
     else:
         localMaxima = 0
-#print breakDownMZ
 
 listofAllTrace = {}
 for mz in breakDownMZ:
     listofAllTrace[mz] = []
-
-
-
 
 #################################
 #BINNING DATA TO UNIT MASS RESOLUTION
@@ -154,31 +173,11 @@ for i in contourz:
 
 newArr = collections.OrderedDict(sorted(tArr.items()))
 
-
-import matplotlib
-import matplotlib.gridspec as gridspec
-import matplotlib.pyplot as plt
-from scipy import *
-from math import log10, floor
-
-def round1sig(yTick):
-    for i in range(0, len(yTick)):
-        #stackoverflow: 3410976
-        if (yTick[i] == 0):
-            continue
-        yTick[i] = round(yTick[i], -int(floor(log10(abs(yTick[i])))))
-    return yTick
-
-def Valueround1sig(yTick):
-    return round(yTick, -int(floor(log10(abs(yTick)))))
-
 showBreakdown = options.breakDown
 line_colours = ('BlueViolet', 'Crimson', 'ForestGreen', 'Indigo', 'Tomato', 'Maroon')
 
 minFilter = int(options.minFilter)
-#print minFilter
 levels = arange(minFilter, max(array(ZMatrix).max(axis=1)), 6)
-#line_widths = (1, 1.5, 2, 2.5, 3, 3.5)
 
 subplotVal = 1
 if(showBreakdown == True):
@@ -199,49 +198,25 @@ ContouryTick = arange(0, Valueround1sig(max(contourPlotArr.keys()))+10, 10)
 
 #Labels the smallest value of the axis and then continue labelling with the arange increment ie 50, 750, 1500, 2250 rather than 50, 800, 1550, 2300 (tl;dr 50 does not affect the arange values)
 ax2.set_yticks(concatenate(([Valueround1sig(min(contourPlotArr.keys()))],ContouryTick[:-1]), axis=0))
-#print ContouryTick[:-1]
 ContourxTick = arange(0, Valueround1sig(max(binArr.keys()))+750, 750)
-#print ContourxTick[:-1]
-#ax2.set_xticks(concatenate(([Valueround1sig(min(binArr.keys()))],ContourxTick[:-1]), axis=0))
 ax2.set_ylabel('Collision Energy (V)')
 ax2.set_xlabel('m/z', style='italic')
 GraphCleanUp(ax2)
 
 
-def normalizeMSSpectrum(mzList, maxValue):
-    relInt = []
-    for i in mzList:
-       relInt.append((float(i)/float(maxValue))*100)
-       #print ((float(i)/float(maxValue))*100)
-    return relInt
-
 ax1 = plt.subplot(gs1[0], sharex=ax2)
-#SummedFullScan = ax1.plot(summed.keys(), summed.values()) #Absolute Intensity
 SummedFullScan = ax1.plot(summed.keys(), normalizeMSSpectrum(summed.values(), max(summed.values())))
 plt.title('Energy Dependent-ESI MS/MS Plot', y=1.08)
 plt.setp(ax1.get_xticklabels(), visible=False)
-#ax1.set_ylabel('Intensity') #Absolute Intensity
 ax1.set_ylabel('Intensity (%)')
 #stackoverflow: 11244514
-#ax1yTick = arange(0, Valueround1sig(max(summed.values()))+10000, 10000) #Absolute Intensity
 ax1.set_yticks([0, 50, 100]) #Relative Intensity`
 GraphCleanUp(ax1)
 ax1.spines['bottom'].set_visible(False)
 ax1.xaxis.set_ticks_position('none')
 
-def RoundToTen(val):
-    import math
-    return int(math.ceil(val/10.0))*10
 
-def RoundToTenFloor(val):
-    import math
-    return int(math.floor(val/10.0))*10
-
-def RoundToHundred(val):
-    import math
-    return int(math.ceil(val/100.0))*100
-
-zoom = True
+zoom = options.zoom 
 if (zoom):
     StartRange = min(listofAllTrace.keys())
     EndRange = max(listofAllTrace.keys())
@@ -252,13 +227,8 @@ if (zoom):
     print RoundToHundred(EndRange)
     xtickRange = arange(RoundToHundred(StartRange)-100, RoundToHundred(EndRange)+100, 100)
     print xtickRange
-    #for i in range(0, len(xtickRange)):
-    #   xtickRange[i] = RoundToTenFloor(xtickRange[i])
-    #print xtickRange
     ax2.set_xticks(xtickRange)
 
-    #xMinVal = RoundToTenFloor(xMinVal)
-    #xMaxVal = RoundToTenFloor(xMaxVal)
     ax2.set_xlim(xmin=xMinVal, xmax=xMaxVal)
     ax1.set_xlim(xmin=xMinVal, xmax=xMaxVal)
 
@@ -271,14 +241,10 @@ if(showBreakdown == True):
         if (testMaxVal > maxOfAllTraces):
             maxOfAllTraces = testMaxVal 
     for mz in breakDownMZ:
-        #ax3.plot(listofAllTrace[mz], contourPlotArr.keys()) #Absolute Intensity
         ax3.plot(normalizeMSSpectrum(listofAllTrace[mz], maxOfAllTraces), contourPlotArr.keys()) 
     plt.setp(ax3.get_yticklabels(), visible=False)
-    #BreakDownInt = arange(0, TraceMaxInt, 1500)
     ax3.set_xlabel('Intensity (%)')
-    #print TraceMaxInt
-    #ax3.set_xticks(BreakDownInt)
-    ax3.set_xticks([' ', ' ',100])
+    ax3.set_xticks([100])
     GraphCleanUp(ax3)
 
 plt.savefig('../{OUTFILE}.png'.format(OUTFILE = options.outputFile), bbox_inches='tight')
